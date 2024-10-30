d3313075cf3024aa2caaa7503ffdb2da49a5aa66
fatmaatef304 <fatimaatef990@gmailcom>
2024-10-30 16:50:27 +0300

    refacotr

--- lib/screens/projects/widgets/project_card.dart
+++ lib/screens/projects/widgets/project_card.dart
@@ -17,71 +17,76 @@ class _ProjectCardState extends State<ProjectCard> {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: SizedBox(
-        width: 300,
-        height: 550,
-        child: Card(
-          color: ColorStyle.light1,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 4,
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      widget.project.img,
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      widget.project.title,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: ColorStyle.primWhite,
+    return GestureDetector(
+      onTap: () {
+        launchurl(widget.project.githubLink);
+      },
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        child: SizedBox(
+          width: 300,
+          height: 550,
+          child: Card(
+            color: ColorStyle.light1,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 4,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        widget.project.img,
                       ),
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(
-                      widget.project.description,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                        color: ColorStyle.secWhite,
-                      ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.code),
-                          onPressed: () {
-                            launchurl(widget.project.githubLink);
-                          },
+                      const SizedBox(height: 8.0),
+                      Text(
+                        widget.project.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: ColorStyle.primWhite,
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              if (_isHovered)
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 69, 200, 187)
-                        .withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text(
+                        widget.project.description,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: ColorStyle.secWhite,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.code),
+                            onPressed: () {
+                              launchurl(widget.project.githubLink);
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-            ],
+                if (_isHovered)
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 69, 200, 187)
+                          .withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       ),
